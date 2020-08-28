2672eaa5bcc52a171d7b2a16054460cbdb2ff080
Ao.Nagasawa <aho_ao1004@yahoo.co.jp>
2020-08-28 13:04:42 +0900

    途中経過

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Ao Nagasawa on 2020/08/27.
+//  Copyright © 2020 Ao.Nagasawa. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
--- Instagram/TabBarController.swift
+++ Instagram/TabBarController.swift
@@ -1,7 +1,18 @@
 import UIKit
+import Firebase
 
 class TabBarController: UITabBarController, UITabBarControllerDelegate {
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // currentUserがnilならログインしていない
+        if Auth.auth().currentUser == nil {
+            // ログインしていないときの処理
+            let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+            self.present(loginViewController!, animated: true, completion: nil)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // タブアイコンの色
@@ -23,6 +34,6 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
             return true
         }
     }
-
-
+    
+    
 }
